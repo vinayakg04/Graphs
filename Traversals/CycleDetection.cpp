92f0a2fc30df89bc9e3dbcf7255cf3a98cd4accb
Vinayak Gupta <vinayakg1256@gmail.com>
2025-01-15 21:13:11 +0530

    Update CycleDetection.cpp

--- Traversals/CycleDetection.cpp
+++ Traversals/CycleDetection.cpp
@@ -1,1 +1,22 @@
-
+queue<pair<int,int>> q;
+        q.push({0,-1}) //{node,parent}
+        
+        while(!q.empty()){
+            auto p=q.front();
+            q.pop();
+            
+            int node=p.first;
+            int parent=p.second;
+            
+            for(auto &v: adj[node]){
+                if(!visited[v]){
+                    visited[v]=true;
+                    q.push({v,node})
+                }else if(v!=parent){
+                    return true; //cycle detected
+                }
+                
+            }
+        }
+        
+        return false;
