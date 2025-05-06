a22ba83eaecf6b116e4c5fa5a0facf48f783010a
Vinayak Gupta <vinayakg1256@gmail.com>
2025-05-06 15:39:03 +0530

    Update LineSweepAlgorithm.cpp

--- LineSweepAlgorithm.cpp
+++ LineSweepAlgorithm.cpp
@@ -1,1 +1,33 @@
+class MyCalendar {
+public:
+    MyCalendar() {
+        
+    }
+    
+    //Double booking not allowed here
+    //Use map because sweep line algorithm works on sorted keys
+    map<int,int> mp;
+    bool book(int startTime, int endTime) {
+        mp[startTime]+=1;
+        mp[endTime]-=1;
 
+        int totalCount=0;
+        for(auto p: mp){
+            totalCount+=p.second;
+
+            if(totalCount>1){
+                mp[startTime]-=1;
+                mp[endTime]+=1;
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
