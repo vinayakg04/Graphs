acd126adf723926fbe9fc66b7d494cdbd833c424
Vinayak Gupta <vinayakg1256@gmail.com>
2025-06-08 17:28:26 +0530

    Update Lexicographical_Numbers.cpp

--- Lexicographical/Lexicographical_Numbers.cpp
+++ Lexicographical/Lexicographical_Numbers.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    void solve(int num,int n,vector<int>& result){
+        if(num>n){
+            return;
+        }
 
+        result.push_back(num);
+
+        //explore all possibilities of num by appending numbers
+
+        for(int append=0;append<=9;append++){
+            int newNum=num*10 + append;
+            if(newNum > n){
+                return;
+            }else{
+                solve(newNum,n,result);
+            }
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        int result=0;
+        for(int num=1;num<=9;num++){
+            solve(num,n,res);
+        }
+
+        return res;
+    }
+};
